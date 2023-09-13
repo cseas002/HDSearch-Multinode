cd80df6a316eddc849aa76069ead21c5d4148fc2
cseas002 <cseas002@ucy.ac.cy>
2023-09-13 17:13:49 -0500

    Added profiler

---
+++ profiler/setHosts.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#!/bin/bash
+
+export ANSIBLE_HOST_KEY_CHECKING=False
+export PROFILER_HOME=/users/cseas002/HDSearch-Multinode/profiler
+
+if [ -z $1 ]; then
+  echo "wrong arguments pass number of nodes to monitor and range"
+  echo "e.g ./setHosts.sh 2 2 4"
+  exit
+else
+  start=$2
+  end=$3
+fi
+
+echo "[profiler]" > hosts
+for i in $(seq $start $end);
+do
+  echo $i
+  echo "node$i" >> hosts
+done
+
+
